0b1d2760ee443e0425bb42cb632ab1d601b4daa5
naweenniroshan <nniroshan@palo-it.com>
2023-09-19 13:28:15 +0800

    add validation - user input can only contains letters

--- src/main/java/com/scrabble/api/dto/SaveScoreRequestDto.java
+++ src/main/java/com/scrabble/api/dto/SaveScoreRequestDto.java
@@ -2,6 +2,7 @@ package com.scrabble.api.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,7 @@ public class SaveScoreRequestDto {
     @NotNull(message = "Word can not be null.")
     @NotEmpty(message = "Word can not be empty.")
     @Size(min = 1, max = 10, message = "Length of the word must be between 1 to 10.")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Word should only contain letters.")
     private String word;
 
 }
